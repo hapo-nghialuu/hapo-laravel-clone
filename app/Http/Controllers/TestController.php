6f5b12bb24b55879bd81eac48f64f11da89d3de8
Luu Nghia <nghialt@haposoft.com>
2020-08-18 13:13:49 +0700

    fix convention

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     //
-    public function test() {
+    public function test()
+    {
         return "Hello world";
     }
 }
